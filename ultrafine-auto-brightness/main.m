fd448a774cf5df0da2bb2286af54d73764ca1a15
Tae Won Ha <h@taewon.de>
2019-03-31 20:39:53 +0200

    Initial Commit

---
+++ ultrafine-auto-brightness/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ultrafine-auto-brightness
+//
+//  Created by Tae Won Ha on 31.03.19.
+//  Copyright © 2019 Tae Won Ha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
